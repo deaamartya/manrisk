9286d87d75261c5c7a8727b40b7e071f7b167c0a
deaamartya <50395247+deaamartya@users.noreply.github.com>
2022-04-20 18:01:36 +0700

    M12 : update mobile css

--- resources/views/layouts/risk-officer/sidebar.blade.php
+++ resources/views/layouts/risk-officer/sidebar.blade.php
@@ -2,7 +2,6 @@
 	<div>
 		<div class="logo-wrapper">
 			<a href="{{ url('/') }}"><img class="img-fluid for-light" src="{{asset('assets/images/logo/logo_company/logo1.png')}}" alt=""><img class="img-fluid for-dark" src="{{asset('assets/images/logo/logo_dark.png')}}" alt=""></a>
-			<div class="back-btn"><i class="fa fa-angle-left"></i></div>
 			<div class="toggle-sidebar"><i class="status_toggle middle sidebar-toggle" data-feather="grid"> </i></div>
 		</div>
 		<div class="logo-icon-wrapper">
@@ -11,7 +10,6 @@
 			</a>
 		</div>
 		<nav class="sidebar-main">
-			<div class="left-arrow" id="left-arrow"><i data-feather="arrow-left"></i></div>
 			<div id="sidebar-menu">
 				<ul class="sidebar-links" id="simple-bar">
 					<li class="back-btn">
@@ -107,7 +105,6 @@
 					</li>
 				</ul>
 			</div>
-			<div class="right-arrow" id="right-arrow"><i data-feather="arrow-right"></i></div>
 		</nav>
 	</div>
 </div>
