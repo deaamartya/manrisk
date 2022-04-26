ba76411f6e00d8ba4a0f82dda6616af1c5e15afb
NyomanErlina <nyoman.erlina19@gmail.com>
2022-04-26 18:27:29 +0700

    M8 : update tampilan responden tabel atas

--- app/Http/Controllers/RiskOfficer/PengukuranRisikoController.php
+++ app/Http/Controllers/RiskOfficer/PengukuranRisikoController.php
@@ -14,47 +14,36 @@ class PengukuranRisikoController extends Controller
 {
     public function index()
     {
-
-        // $qcsrisk = "SELECT * FROM s_risiko WHERE id_user='$id_user' AND tahun='2022' AND status_s_risiko=1 ";
-        // $csrisk = mysqli_query($connect, $qcsrisk);
-        // $jris=mysqli_num_rows($csrisk);
-        
-        // $querysr = "SELECT * FROM s_risiko WHERE id_user='$id_user' AND tahun='2022' AND status_s_risiko=1 LIMIT 1";
-        // $sqlsr = mysqli_query($connect, $querysr);
-        // $datasr = mysqli_fetch_assoc($sqlsr);
-
         //ID USER GANTI PAKE AUTH SESSION LOGIN !!!!!!!!
+        // $user =  Auth::user()->id_user
         $user = 1;
         $jml_risk = Srisiko::where('id_user', $user)->where('tahun', '2022')->where('status_s_risiko', 1)->count();
         $data_sr = Srisiko::where('id_user', $user)->where('tahun', '2022')
-                            ->where('status_s_risiko', 1)->pluck('id_s_risiko');
+                            ->where('status_s_risiko', 1)->limit(1)->get();
+                            
+        // dd($data_sr);
+        foreach($data_sr as $d){
+            $pengukuran = Pengukuran::join('s_risiko', 'pengukuran.id_s_risiko', 's_risiko.id_s_risiko')
+                                        ->where('pengukuran.id_s_risiko', $d->id_s_risiko)->get();
+                // dd($pengukuran);
+            //Company ID GANTI PAKE AUTH SESSION LOGIN !!!!!!!!
+            $company_id = 'PI';
+            $jabatan = DefendidPengukur::where('company_id', $company_id)->get();
 
-        // $queryn = "SELECT * FROM pengukuran p INNER JOIN s_risiko sr ON p.id_s_risiko=sr.id_s_risiko WHERE p.id_s_risiko = '$datasr[id_s_risiko]'";
-        
-        $pengukuran = Pengukuran::join('s_risiko', 'pengukuran.id_s_risiko', 's_risiko.id_s_risiko')
-                                    ->where('pengukuran.id_s_risiko', $data_sr)->get();
+            $arr_pengukur = [];
+            foreach($jabatan as $i=>$j){
+                $pengukur_risk = Pengukuran::join('s_risiko', 'pengukuran.id_s_risiko', 's_risiko.id_s_risiko')
+                ->where('pengukuran.id_s_risiko', $d->id_s_risiko)
+                ->where('pengukuran.nama_responden', $j->jabatan)
+                ->get();  
 
-        // $query_jab = "SELECT * FROM defendid_pengukur where company_id='$company_id'";
-        //Company ID GANTI PAKE AUTH SESSION LOGIN !!!!!!!!
-        $company_id = 'PI';
-        $jabatan = DefendidPengukur::where('company_id', $company_id)->get();
-
-        // $query_cek = "SELECT * FROM pengukuran p INNER JOIN s_risiko sr ON p.id_s_risiko=sr.id_s_risiko WHERE p.id_s_risiko = '$datasr[id_s_risiko]' AND p.nama_responden = '$jabatan[jabatan]'"; 
-        foreach($jabatan as $j){
-            $cek_pengukuran = Pengukuran::join('s_risiko', 'pengukuran.id_s_risiko', 's_risiko.id_s_risiko')
-            ->where('pengukuran.id_s_risiko', $data_sr)
-            ->where('pengukuran.nama_responden', $j->jabatan)
-            ->get();  
+                if(count($pengukur_risk) == 0){
+                        $arr_pengukur[] = $j;
+                }
+                    
+            }
+        // dd(count($arr_pengukur));
         }
-
-        // $query2 = "SELECT *
-        // FROM PENGUKURAN A 
-        // INNER JOIN s_risiko B ON A.id_s_risiko=B.id_s_risiko 
-        // INNER JOIN konteks C ON B.id_konteks=C.id_konteks 
-        // INNER JOIN defendid_pengukur D ON A.id_pengukur=D.id_pengukur
-        // INNER JOIN defendid_user E ON D.company_id=E.company_id
-        // WHERE E.id_user=$id_user AND A.tahun_p='2022'
-        // AND B.status_s_risiko=1";
         $sumber_risiko = Pengukuran::join('s_risiko', 'pengukuran.id_s_risiko', 's_risiko.id_s_risiko')
             ->join('konteks', 's_risiko.id_konteks', 'konteks.id_konteks')
             ->join('defendid_pengukur', 'pengukuran.id_pengukur', 'defendid_pengukur.id_pengukur')
@@ -64,7 +53,7 @@ class PengukuranRisikoController extends Controller
             ->where('s_risiko.status_s_risiko', 1)
             ->get();
 
-        return view('risk-officer.pengukuran-risiko', compact('jml_risk','data_sr','pengukuran','jabatan','sumber_risiko', 'cek_pengukuran'));
+        return view('risk-officer.pengukuran-risiko', compact('jml_risk','data_sr','jabatan','pengukuran','arr_pengukur','sumber_risiko'));
     }
 
 
