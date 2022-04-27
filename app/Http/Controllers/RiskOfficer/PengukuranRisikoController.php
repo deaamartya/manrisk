3e7e514d588aee63afb1aa8c797d01039222e8c1
NyomanErlina <nyoman.erlina19@gmail.com>
2022-04-27 08:16:55 +0700

    M8 : update tampilan pengukuran risiko dan print pdf

--- app/Http/Controllers/RiskOfficer/PengukuranRisikoController.php
+++ app/Http/Controllers/RiskOfficer/PengukuranRisikoController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use App\Models\SRisiko;
 use App\Models\Risk;
 use PDF;
+use DB;
 
 class PengukuranRisikoController extends Controller
 {
@@ -24,8 +25,8 @@ class PengukuranRisikoController extends Controller
         // dd($data_sr);
         foreach($data_sr as $d){
             $pengukuran = Pengukuran::join('s_risiko', 'pengukuran.id_s_risiko', 's_risiko.id_s_risiko')
-                                        ->where('pengukuran.id_s_risiko', $d->id_s_risiko)->get();
-                // dd($pengukuran);
+                        ->where('pengukuran.id_s_risiko', $d->id_s_risiko)->get();
+               
             //Company ID GANTI PAKE AUTH SESSION LOGIN !!!!!!!!
             $company_id = 'PI';
             $jabatan = DefendidPengukur::where('company_id', $company_id)->get();
@@ -58,56 +59,18 @@ class PengukuranRisikoController extends Controller
 
 
     public function generatePDF()
-    {
-        // $data = [
-        //     'title' => 'Cetak Penilaian',
-        //     'date' => date('d/m/Y')
-        // ];
-          
-        // $pdf = PDF::loadView('risk-officer.form_kompilasi', $data);
-    
-        // return $pdf->download('cetak penilaian.pdf');
-        
-        // if(isset($_GET['id'])){
-        //     $id = $_GET['id'];
-        
-        //     $query1 = "SELECT * FROM risk_header r INNER JOIN divisi d WHERE d.id_divisi = r.id_divisi AND r.id_riskh='$id'";
-        //     // echo $query1;
-        //     $sql1 = mysqli_query($connect, $query1);
-        //     if ($sql1 && mysqli_num_rows($sql1) > 0) {
-        //         $datarh = mysqli_fetch_assoc($sql1);
-        //     }
-            
-        //     $query2 = "SELECT * FROM risk_detail d INNER JOIN s_risiko s INNER JOIN divisi di INNER JOIN risk_header h INNER JOIN konteks k WHERE d.id_s_risiko=s.id_s_risiko AND d.id_riskh=h.id_riskh AND s.id_divisi=di.id_divisi AND s.id_konteks=k.id_konteks AND d.id_riskh='$id' ORDER BY d.id_riskd";
-            
-        //     $query3 = "SELECT C.id_risk, C.konteks, B.s_risiko, AVG(A.nilai_L) AS l, AVG(A.nilai_C) AS c, AVG(A.nilai_L)*AVG(A.nilai_C) AS r, count(A.nama_responden)
-        //     FROM PENGUKURAN A 
-        //     INNER JOIN s_risiko B ON A.id_s_risiko=B.id_s_risiko 
-        //     INNER JOIN konteks C ON B.id_konteks=C.id_konteks 
-        //     INNER JOIN defendid_pengukur D ON A.id_pengukur=D.id_pengukur
-        //     WHERE A.tahun_p='2022'
-        //     AND B.status_s_risiko=1
-        //     group by C.id_risk, C.konteks, B.s_risiko, B.id_s_risiko";
-        //     $sql3 = mysqli_query($connect, $query3);
-        
-        //     $sql2 = mysqli_query($connect, $query2);
-        
-        //     include(dirname(__FILE__).'/form_kompilasi.php');
-        
-        //     $content = ob_get_clean();
-        
-        //     // conversion HTML => PDF
-        //     require_once('../html2pdf/html2pdf.class.php');
-        //     try
-        //     {
-        //         $html2pdf = new HTML2PDF('L','A4', 'En', false, 'ISO-8859-15');
-        //         $html2pdf->setTestTdInOnePage(false);
-        //         $html2pdf->setDefaultFont('Arial');	
-        //         $html2pdf->writeHTML($content, isset($_GET['vuehtml']));
-        //         $html2pdf->Output('Wifi_'.$area.'_Sesi'.$sesi.'.pdf');//output laporan setelah di download
-        //     }
-        //     catch(HTML2PDF_exception $e) { echo $e; }
-        // }
+    {   
+        $data = Pengukuran::select('k.id_risk', 'k.konteks', 'sr.s_risiko', DB::raw('AVG(pengukuran.nilai_L) as L'), DB::raw('AVG(pengukuran.nilai_C) as C'), DB::raw('AVG(pengukuran.nilai_L) * AVG(pengukuran.nilai_C) as R'), DB::raw('count(pengukuran.nama_responden)'))
+                ->join('s_risiko as sr', 'pengukuran.id_s_risiko', 'sr.id_s_risiko')
+                ->join('konteks as k', 'sr.id_konteks', 'k.id_konteks')
+                ->join('defendid_pengukur as d', 'pengukuran.id_pengukur', 'd.id_pengukur')
+                ->where('pengukuran.tahun_p', '2022')
+                ->where('sr.status_s_risiko', '1')
+                ->groupBy('k.id_risk', 'k.konteks',  'sr.s_risiko', 'sr.id_s_risiko')
+                ->get();
+        $pdf = PDF::loadView('risk-officer.form_kompilasi', compact('data'))->setPaper( 'a4','landscape');
+        return $pdf->stream('Hasil Kompilasi Risiko.pdf');
+
     }
 
 }
