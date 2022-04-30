83d55b15c00be6112402ab4cbc9ce6b1da488fe9
deaamartya <50395247+deaamartya@users.noreply.github.com>
2022-05-01 01:28:38 +0700

    M10 : print risk header

--- app/Http/Controllers/RiskOfficer/RisikoController.php
+++ app/Http/Controllers/RiskOfficer/RisikoController.php
@@ -9,7 +9,6 @@ use App\Models\DefendidUser;
 use Barryvdh\DomPDF\Facade\Pdf as DomPDF;
 use Auth;
 use PDF;
-use App\Models\SRisiko;
 
 class RisikoController extends Controller
 {
@@ -35,8 +34,7 @@ class RisikoController extends Controller
         RiskHeader::insert([
             'id_user' => Auth::user()->id_user,
             'tahun' => $request->tahun,
-            'target' => $request->target,
-            'penyusun' => Auth::user()->nama,
+            'target' => $request->target
         ]);
         return redirect()->route('risk-officer.risiko.index')->with(['success-swal' => 'Risk Header berhasil disimpan!']);
     }
@@ -50,13 +48,7 @@ class RisikoController extends Controller
     public function show($id)
     {
         $headers = RiskHeader::where('id_riskh', '=', $id)->first();
-        $pilihan_s_risiko = SRisiko::where([
-            ['id_user', '=', Auth::user()->id_user],
-            ['tahun', '=', date('Y')],
-            ['status_s_risiko', '=', 1],
-            ['company_id', '=', Auth::user()->company_id],
-        ])->orderBy('id_s_risiko')->get();
-        return view('risk-officer.detail-risiko', compact("headers", 'pilihan_s_risiko'));
+        return view('risk-officer.detail-risiko', compact("headers"));
     }
 
     /**
@@ -73,6 +65,7 @@ class RisikoController extends Controller
             'tahun' => $request->tahun,
             'target' => $request->target
         ]);
+        dd($riskheader);
         return redirect()->route('risk-officer.risiko.index')->with(['success-swal' => 'Risk Header berhasil diubah!']);
     }
 
@@ -94,17 +87,5 @@ class RisikoController extends Controller
         // return view('risk-officer.risk-header-pdf', compact('header', 'user'));
         $pdf = PDF::loadView('risk-officer.risk-header-pdf', compact('header', 'user'))->setPaper('a4', 'landscape');
         return $pdf->stream('Laporan Manajemen Risiko '.$user->instansi.' Tahun '.$header->tahun.'.pdf');
-    }
-
-    public function uploadLampiran(Request $request) {
-        $id = $request->id;
-        $riskheader = RiskHeader::where('id_riskh', '=', $id)->first();
-        $filename = $request->file('lampiran')->getClientOriginalName();
-        $folder = '/document/lampiran/';
-        $request->file('lampiran')->storeAs($folder, $filename, 'public');
-        $riskheader->update([
-            'lampiran' => $filename,
-        ]);
-        return redirect()->route('risk-officer.risiko.show', $id)->with(['success-swal' => 'Lampiran berhasil diupload!']);
     }
 }
--- routes/risk-officer.php
+++ routes/risk-officer.php
@@ -5,11 +5,8 @@ use \App\Http\Controllers\RiskOfficer\{
   HomeController,
   SumberRisikoController,
   UserController,
-  PengukuranRisikoController,
   RisikoController,
-  PengajuanMitigasiController,
-  RiskDetailController,
-  MitigasiPlanController
+  PengukuranRisikoController,
 };
 
 Route::middleware(['auth', 'cekRiskOfficer'])->name('risk-officer.')->group(function () {
@@ -17,16 +14,12 @@ Route::middleware(['auth', 'cekRiskOfficer'])->name('risk-officer.')->group(func
   Route::get('user/get-user/{id}', [UserController::class, 'get_user'])->name('user-get-user');
   Route::post('user/store/{id?}', [UserController::class, 'store'])->name('user-store');
   Route::put('user/update-status/{id}', [UserController::class, 'update_status'])->name('user-update-status');
-  Route::get('risiko/print/{id}', [RisikoController::class, 'print'])->name('risiko.print');
-  Route::resource('sumber-risiko', SumberRisikoController::class);
+  Route::resource('sumber-risiko',SumberRisikoController::class);
   Route::get('pengukuran-risiko', [PengukuranRisikoController::class, 'index'])->name('pengukuran-risiko');
   Route::get('pengukuran-risiko-indhan', [PengukuranRisikoIndhanController::class, 'index'])->name('pengukuran-risiko-indhan');
   Route::resource('risiko', RisikoController::class);
   Route::get('risiko/print/{id}', [RisikoController::class, 'print'])->name('risiko.print');
-  Route::post('risiko/upload-lampiran', [RisikoController::class, 'uploadLampiran'])->name('risiko.upload-lampiran');
-  Route::resource('pengajuan-mitigasi', PengajuanMitigasiController::class);
-  Route::resource('risk-detail', RiskDetailController::class);
-  Route::resource('mitigasi-plan', MitigasiPlanController::class);
+  Route::get('mitigasi-plan', [HomeController::class, 'index'])->name('mitigasi-plan');
   Route::get('kuesioner', [HomeController::class, 'index'])->name('kuesioner');
   Route::get('table', [HomeController::class, 'table'])->name('table');
   Route::get('form', [HomeController::class, 'form'])->name('form');
