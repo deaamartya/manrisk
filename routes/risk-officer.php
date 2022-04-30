8424c45f1bf55187f99cc9c64c0f7442947ff2b2
deaamartya <50395247+deaamartya@users.noreply.github.com>
2022-05-01 01:28:38 +0700

    M10 : print risk header

--- routes/risk-officer.php
+++ routes/risk-officer.php
@@ -6,11 +6,8 @@ use \App\Http\Controllers\RiskOfficer\{
   SumberRisikoController,
   PengukuranRisikoController
   UserController,
-  PengukuranRisikoController,
   RisikoController,
-  PengajuanMitigasiController,
-  RiskDetailController,
-  MitigasiPlanController
+  PengukuranRisikoController,
 };
 
 Route::middleware(['auth', 'cekRiskOfficer'])->name('risk-officer.')->group(function () {
@@ -18,8 +15,7 @@ Route::middleware(['auth', 'cekRiskOfficer'])->name('risk-officer.')->group(func
   Route::get('user/get-user/{id}', [UserController::class, 'get_user'])->name('user-get-user');
   Route::post('user/store/{id?}', [UserController::class, 'store'])->name('user-store');
   Route::put('user/update-status/{id}', [UserController::class, 'update_status'])->name('user-update-status');
-  Route::get('risiko/print/{id}', [RisikoController::class, 'print'])->name('risiko.print');
-  Route::resource('sumber-risiko', SumberRisikoController::class);
+  Route::resource('sumber-risiko',SumberRisikoController::class);
   Route::get('pengukuran-risiko', [PengukuranRisikoController::class, 'index'])->name('pengukuran-risiko');
   Route::get('generate-pdf', [PengukuranRisikoController::class, 'generatePDF'])->name('pengukuran-generatePDF');
   Route::post('penilaian-risiko', [PengukuranRisikoController::class, 'penilaianRisiko'])->name('penilaian-risiko');
@@ -28,10 +24,7 @@ Route::middleware(['auth', 'cekRiskOfficer'])->name('risk-officer.')->group(func
   Route::get('pengukuran-risiko-indhan', [PengukuranRisikoIndhanController::class, 'index'])->name('pengukuran-risiko-indhan');
   Route::resource('risiko', RisikoController::class);
   Route::get('risiko/print/{id}', [RisikoController::class, 'print'])->name('risiko.print');
-  Route::post('risiko/upload-lampiran', [RisikoController::class, 'uploadLampiran'])->name('risiko.upload-lampiran');
-  Route::resource('pengajuan-mitigasi', PengajuanMitigasiController::class);
-  Route::resource('risk-detail', RiskDetailController::class);
-  Route::resource('mitigasi-plan', MitigasiPlanController::class);
+  Route::get('mitigasi-plan', [HomeController::class, 'index'])->name('mitigasi-plan');
   Route::get('kuesioner', [HomeController::class, 'index'])->name('kuesioner');
   Route::get('table', [HomeController::class, 'table'])->name('table');
   Route::get('form', [HomeController::class, 'form'])->name('form');
