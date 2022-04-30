37a83cbe9b289af24f106123f18868474635689b
deaamartya <50395247+deaamartya@users.noreply.github.com>
2022-04-30 19:14:03 +0700

    M7 : update css

--- routes/risk-officer.php
+++ routes/risk-officer.php
@@ -10,7 +10,7 @@ Route::name('risk-officer.')->group(function () {
   Route::resource('sumber-risiko',SumberRisikoController::class);
   Route::get('pengukuran-risiko', [PengukuranRisikoController::class, 'index'])->name('pengukuran-risiko');
   Route::get('pengukuran-risiko-indhan', [PengukuranRisikoIndhanController::class, 'index'])->name('pengukuran-risiko-indhan');
-  Route::get('risiko', [HomeController::class, 'index'])->name('risiko');
+  Route::resource('risiko', HomeController::class);
   Route::get('mitigasi-plan', [HomeController::class, 'index'])->name('mitigasi-plan');
   Route::get('kuesioner', [HomeController::class, 'index'])->name('kuesioner');
   Route::get('table', [HomeController::class, 'table'])->name('table');
