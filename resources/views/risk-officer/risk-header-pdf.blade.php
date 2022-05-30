c5d65b66f369bfba4667ff09f7bd47c180f3bf4b
deaamartya <50395247+deaamartya@users.noreply.github.com>
2022-05-30 09:49:33 +0700

    Revert "update print mitigasi plan"
    This reverts commit 64230cd0df8db30f6e786e879fe63f754add8472.

--- resources/views/risk-officer/risk-header-pdf.blade.php
+++ resources/views/risk-officer/risk-header-pdf.blade.php
@@ -88,7 +88,7 @@
 		// var 0 = tanggal
 		// var 1 = bulan
 		// var 2 = tahun
-	}
+	}		
 	@endphp
     <table class="table-header" cellspacing="0" width="100%">
     	<tr>
@@ -96,8 +96,7 @@
     			<img src="{{ $_SERVER['DOCUMENT_ROOT'].'/assets/images/logo/logo_company/logo_bumn.png' }}" style="width:90px;" />
     		</td>
     		<td width="70%" height="40">
-    			<b>RENCANA PENGELOLAAN RISIKO (MITIGASI PLAN)</b>
-    			{{-- <b>MITIGASI PLAN {{ $user->perusahaan->instansi }} </b> --}}
+    			<b>RISK REGISTER {{ $user->perusahaan->instansi }} </b>
     		</td>
     		<td width="143">
     			<img src="{{ $_SERVER['DOCUMENT_ROOT'].'/assets/images/logo/logo_company/logo_'.$user->perusahaan->company_code.'.png' }}" style="width:120px;" />
@@ -154,99 +153,192 @@
     		</td>
     	</tr>
     </table>
-	<table class="table-4" cellspacing="0" width="100%">
+		<table class="table-4" cellspacing="0" width="100%">
     	<tr>
-    		<td width="5%" height="70" class="center f-11">
+    		<td class="f-13 center" colspan="7">
+    			IDENTIFIKASI
+    		</td>
+    		<td class="f-13 center" colspan="7">
+    			PENGENDALIAN DAN PENILAIAN AWAL
+    		</td>
+    		<td class="f-13 center" colspan="2">
+    			PENANGANAN
+    		</td>
+    		<td class="f-13 center" colspan="4">
+    			PENILAIAN AKHIR
+    		</td>
+    	</tr>
+    	<tr>
+    		<td width="5%" height="70" class="center f-11" rowspan="2">
     			ID Risk
     		</td>
-    		<td width="7%" class="center f-12">
-    			Peristiwa yang mengganggu target / sasaran
+    		<td width="7%" class="center f-12" rowspan="2">
+    			Konteks Organisasi
     		</td>
-    		<td width="8%" class="center f-10">
+    		<td width="8%" class="center f-10" rowspan="2">
+    			Persyaratan Perundangan, Kebutuhan dan Harapan
+    		</td>
+    		<td width="8%" class="center f-12" rowspan="2">
+    			Indikator Risiko
+    		</td>
+    		<td width="6%" class="center f-12" rowspan="2">
+    			Risiko
+    		</td>
+    		<td width="6%" class="center f-12" rowspan="2">
+    			Penyebab Risiko
+    		</td>
+    		<td width="6%" class="center f-12" rowspan="2">
+    			Dampak Risiko
+    		</td>
+    		<td width="3%" class="center f-11" rowspan="2">
+    			UC/C
+    		</td>
+    		<td width="7%" class="center f-11" rowspan="2">
+    			Pengendalian Risiko Saat Ini
+    		</td>
+    		<td width="10%" class="center f-11 p-0" colspan="3">
     			Level Risiko Awal
     		</td>
-    		<td width="8%" class="center f-12">
-    			Opsi Mitigasi
+    		<td width="8%" class="center f-11" rowspan="2">
+    			PELUANG
     		</td>
-    		<td width="6%" class="center f-12">
-    			Penanggung Jawab
+    		<td width="6%" class="center f-11" rowspan="2">
+    			Rencana Tindak Lanjut
     		</td>
-    		<td width="6%" class="center f-12">
-    			Jadwal Pelaksanaan
+    		<td width="6%" class="center f-10" rowspan="2">
+    			Jadwal Pelaksan aan
     		</td>
-    		<td width="6%" class="center f-12">
-    			% Realisasi
+    		<td width="4%" class="center f-11" rowspan="2">
+    			PIC
     		</td>
-    		<td width="3%" class="center f-11">
+    		<td width="8%" class="center f-11" rowspan="2">
+    			Dokumen Terkait
+    		</td>
+    		<td width="10%" class="center f-11 p-0" colspan="3">
     			Level Risiko Akhir
     		</td>
-    		<td width="7%" class="center f-11">
-    			Keterangan
-    		</td>
     	</tr>
+			<tr class="custom-tr">
+				<td style="height: 10px;" width="2%" class="center p-0 f-10">L</td>
+				<td style="height: 10px;" width="2%" class="center p-0 f-10">C</td>
+				<td style="height: 10px;" width="3%" class="center p-0 f-10">R</td>
+				<td style="height: 10px;" width="2%" class="center p-0 f-10">L</td>
+				<td style="height: 10px;" width="2%" class="center p-0 f-10">C</td>
+				<td style="height: 10px;" width="4.5%" class="center p-0 f-10">R</td>
+			</tr>
     	<tr>
-    		<td class="center f-11">
+    		<td width="3%" class="center f-11">
     			(1)
     		</td>
-    		<td class="center f-12">
+    		<td width="6%" class="center f-12">
     			(2)
     		</td>
-    		<td class="center f-10">
+    		<td width="7%" class="center f-10">
     			(3)
     		</td>
-    		<td class="center f-12">
+    		<td width="6%" class="center f-12">
     			(4)
     		</td>
-    		<td class="center f-12">
+    		<td width="6%" class="center f-12">
     			(5)
     		</td>
-    		<td class="center f-12">
+    		<td width="5.9%" class="center f-12">
     			(6)
     		</td>
-    		<td class="center f-12">
+    		<td width="5.9%" class="center f-12">
     			(7)
     		</td>
-    		<td class="center f-11">
+    		<td width="2.6%" class="center f-11">
     			(8)
     		</td>
-    		<td class="center f-11">
+    		<td width="7%" class="center f-11">
     			(9)
     		</td>
+    		<td width="6%" class="center f-11 p-0"  colspan="3">
+    			(10)
+    		</td>
+    		<td width="6.7%" class="center f-11">
+    			(11)
+    		</td>
+    		<td width="7%" class="center f-11">
+    			(12)
+    		</td>
+    		<td width="4.3%" class="center f-10">
+    			(13)
+    		</td>
+    		<td width="7%" class="center f-11">
+    			(14)
+    		</td>
+    		<td width="6%" class="center f-11">
+    			(15)
+    		</td>
+    		<td width="7%" class="center f-11 p-0"  colspan="3">
+    			(16)
+    		</td>
     	</tr>
-		@foreach($header->risk_detail as $rd)
-		<tr class="content">
-			<td width="5%" class="center f-11">
-				{{ $rd->sumber_risiko->konteks->id_risk }}
-			</td>
-			<td class="center f-11">
-                {{ $rd->indikator }}
-			</td>
-            <td class="center f-11">
-                {{ round(($rd->l_awal*$rd->c_awal), 0) }}
-            </td>
-			<td width="6%" class="center f-11">
-				{!! nl2br($rd->tindak_lanjut) !!}
-			</td>
-			<td width="4%" class="center f-11">
-				{{ $rd->pic }}
-			</td>
-			<td width="6%" class="center f-10">
-                @if($rd->jadwal_mitigasi)
-				    {{ date('d F Y', $rd->jadwal_mitigasi) }}
-                @else
-                    -
-                @endif
-			</td>
-            <td class="center f-11">
-                {{ $rd->realisasi }}
-            </td>
-            <td class="center f-11">
-                {{ round(($rd->l_akhir*$rd->c_akhir), 0) }}
-            </td>
-            <td class="center f-11">
-                {{ $rd->keterangan }}
-            </td>
-		</tr>
-		@endforeach
+			@foreach($header->risk_detail as $rd)
+			<tr class="content">
+    		<td width="5%" class="center f-11">
+    			{{ $rd->sumber_risiko->konteks->id_risk }}
+    		</td>
+    		<td width="7%" class="center f-12">
+    			{{ $rd->sumber_risiko->konteks->konteks }}
+    		</td>
+    		<td width="8%" class="center f-10">
+    			{{ $rd->ppkh }}
+    		</td>
+    		<td width="8%" class="center f-12">
+    			{!! wordwrap(nl2br($rd->indikator), 14, '<br />', true) !!}
+    		</td>
+    		<td width="6%" class="center f-12">
+    			{!! wordwrap(nl2br($rd->sumber_risiko->s_risiko), 14, '<br />', true) !!}
+    		</td>
+    		<td width="6%" class="center f-12">
+    			{!! wordwrap(nl2br($rd->sebab), 14, '<br />', true) !!}
+    		</td>
+    		<td width="6%" class="center f-12">
+    			{!! nl2br($rd->dampak) !!}
+    		</td>
+    		<td width="3%" class="center f-11">
+    			{{ $rd->uc }}
+    		</td>
+    		<td width="7%" class="center f-11">
+    			{!! wordwrap(nl2br($rd->pengendalian), 14, '<br />', true) !!}
+    		</td>
+    		<td width="2%" class="center f-11">
+    			{{ $rd->l_awal }}
+    		</td>
+				<td width="2%" class="center f-11">
+    			{{ $rd->c_awal }}
+    		</td>
+				<td width="3%" class="center f-11">
+    			{{ $rd->r_awal }}
+    		</td>
+    		<td width="8%" class="center f-11">
+    			{!! nl2br($rd->peluang) !!}
+    		</td>
+    		<td width="6%" class="center f-11">
+    			{!! nl2br($rd->tindak_lanjut) !!}
+    		</td>
+    		<td width="6%" class="center f-10">
+    			{!! wordwrap(nl2br($rd->jadwal), 10, '<br />', true) !!}
+    		</td>
+    		<td width="4%" class="center f-11">
+    			{{ $rd->pic }}
+    		</td>
+    		<td width="8%" class="center f-11">
+    			{!! nl2br($rd->dokumen) !!}
+    		</td>
+    		<td width="2%" class="center f-11">
+    			{{ $rd->l_akhir }}
+    		</td>
+				<td width="2%" class="center f-11">
+    			{{ $rd->c_akhir }}
+    		</td>
+				<td width="3%" class="center f-11">
+    			{{ $rd->r_akhir }}
+    		</td>
+    	</tr>
+			@endforeach
     </table>
 </body>
