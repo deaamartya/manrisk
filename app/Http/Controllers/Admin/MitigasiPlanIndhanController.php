ad9ca83424fbb07e17f9d8d9a037ae476a10a0ef
deaamartya <50395247+deaamartya@users.noreply.github.com>
2022-06-20 23:40:30 +0700

    M14 : revisi dea selesai

---
+++ app/Http/Controllers/Admin/MitigasiPlanIndhanController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\RiskHeaderIndhan;
+use App\Models\RiskDetail;
+use MitigasiLogs;
+use Redirect;
+
+class MitigasiPlanIndhanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $headers = RiskHeaderIndhan::all();
+        $jml_risk = [];
+        foreach($headers as $h) {
+            $jml_risk[] = RiskDetail::where('tahun', '=', $h->tahun)
+                ->where('status_indhan', '=', 1)
+                ->count();
+        }
+        return view('admin.mitigasi-plan-indhan', compact('headers', 'jml_risk'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $headers = RiskHeaderIndhan::where('id_riskh', '=', $id)->first();
+        return view('admin.detail-mitigasi-plan-indhan', compact("headers"));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function getProgressData(Request $request) {
+        $data = null;
+        $logs = MitigasiLogs::where('id_riskd', '=', $request->id)->orderBy('created_at', 'DESC')->get();
+        if($logs != null){
+            $data = new \stdClass();
+            $data->data = [];
+            $count = 0;
+            foreach($logs as $c){
+                if ($c->dokumen == null) {
+                    $isi = [
+                        $count + 1,
+                        $c->realisasi,
+                        date('d M Y', strtotime($c->created_at)),
+                        '',
+                        $c->description ? $c->description : '-'
+                    ];
+                } else {
+                    $path = asset('document/mitigasi-progress/'. $c->dokumen);
+                    $isi = [
+                        $count + 1,
+                        $c->realisasi,
+                        date('d M Y', strtotime($c->created_at)),
+                        '<a href="'. $path. '"  target="_blank" class="btn btn-xs btn-info p-1">Lihat Dokumen</a>',
+                        $c->description ? $c->description : '-'
+                    ];
+                }
+                array_push($data->data, $isi);
+                $count++;
+            }
+        }
+        return response()->json($data);
+    }
+
+    public function insertProgress(Request $request) {
+        $filename = null;
+        if($request->dokumen) {
+            $filename = $request->file('dokumen')->getClientOriginalName();
+            $folder = '/document/mitigasi-progress/';
+            $request->file('dokumen')->storeAs($folder, $filename, 'public');
+        }
+        MitigasiLogs::insert([
+            'id_riskd' => $request->id_riskd,
+            'id_user' => $request->id_user,
+            'realisasi' => $request->prosentase,
+            'dokumen' => $filename,
+            'description' => $request->description,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        return Redirect::back()->with(['success-swal' => 'Progress Mitigasi berhasil ditambahkan.']);
+    }
+}
--- app/Models/RiskHeaderIndhan.php
+++ app/Models/RiskHeaderIndhan.php
@@ -9,6 +9,7 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use DB;
 
 /**
  * Class RiskHeaderIndhan
@@ -57,5 +58,53 @@ class RiskHeaderIndhan extends Model
 	public function perusahaan()
 	{
 		return $this->belongsTo(Perusahaan::class, 'company_id');
+	}
+
+	public function risk_detail()
+	{
+		$details = RiskDetail::where('status_mitigasi', '=', 1)
+			->where('tahun', '=', $this->tahun)
+			->where('status_indhan', '=', 1)
+			->whereNull('deleted_at')
+			->get();
+		return $details;
+	}
+
+	public function getMitigasiDetail() {
+		$mitigasi_logs = DB::raw("(
+				SELECT MAX(realisasi) as final_realisasi, id_riskd FROM mitigasi_logs WHERE is_approved = 1 ORDER BY updated_at DESC
+		) as mitigasi_logs");
+		$details = RiskDetail::join('s_risiko as sr', 'sr.id_s_risiko', '=', 'risk_detail.id_s_risiko')
+			->join('konteks as k', 'k.id_konteks', '=', 'sr.id_konteks')
+			->leftJoin($mitigasi_logs, 'mitigasi_logs.id_riskd', 'risk_detail.id_riskd')
+			->where('status_mitigasi', '=', 1)
+			->where('risk_detail.tahun', '=', $this->tahun)
+			->where('status_indhan', '=', 1)
+			->whereNull('risk_detail.deleted_at')
+			->get();
+		return $details;
+	}
+
+	public function migrateCount($id)
+	{
+		$jml = RiskDetail::where('status_mitigasi', '=', 1)
+			->where('risk_detail.tahun', '=', $this->tahun)
+			->where('status_indhan', '=', 1)
+			->whereNull('deleted_at')
+			->count('id_riskd');
+		return $jml;
+	}
+
+	public function doneMigrateCount($id)
+	{
+		$jml = RiskDetail::join('mitigasi_logs as l', 'l.id_riskd', '=', 'risk_detail.id_riskd')
+			->where('status_mitigasi', '=', 1)
+			->where('tahun', '=', $this->tahun)
+			->where('status_indhan', '=', 1)
+			->where('l.realisasi', '=', 100)
+			->where('l.is_approved', '=', 1)
+			->whereNull('risk_detail.deleted_at')
+			->count('risk_detail.id_riskd');
+		return $jml;
 	}
 }
