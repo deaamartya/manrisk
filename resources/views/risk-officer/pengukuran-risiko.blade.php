c804761e031bc8a454c40aeeee160cc042f75111
deaamartya <50395247+deaamartya@users.noreply.github.com>
2022-06-21 14:12:25 +0700

    M14 : fix error responden kosong

--- resources/views/risk-officer/pengukuran-risiko.blade.php
+++ resources/views/risk-officer/pengukuran-risiko.blade.php
@@ -148,7 +148,7 @@
                                                         <label class="col-md-3 col-sm-3 col-xs-12" for="noarsip">Nama Responden <span class="required"></span></label>
                                                         <div class='col-md-9 col-sm-9 col-xs-12'>
                                                         <input type="text" name="nama_responden" style="width: 100%;" required="required" class="form-control " readonly value="{{ Auth::user()->defendid_pengukur->jabatan }}">
-                                                        <input type="hidden" name="id_responden" required="required" value="{{ $p->id_pengukur }}">
+                                                        <input type="hidden" name="id_responden" required="required" value="{{ Auth::user()->defendid_pengukur->id_pengukur }}">
                                                         </div>
                                                     </div>
                                                     <div class="row mb-3">
